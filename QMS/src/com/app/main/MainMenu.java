402a3174aa6289f6c02e713272d1ab763175850f
dktecollege-ac <dktecollege@gmail.com>
2025-12-18 12:23:51 +0530

    only comment added

--- QMS/src/com/app/main/MainMenu.java
+++ QMS/src/com/app/main/MainMenu.java
@@ -1,10 +1,13 @@
+
 package com.app.main;
 
 public class MainMenu {
 
 	public static void main(String[] args) {
 		System.out.println("QMS PROJECT");
+		System.out.println("mainmenu");
 
 	}
 
 }
+//my branch code
